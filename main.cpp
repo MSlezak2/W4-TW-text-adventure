895fa03c7964acb5b379343a41e920890a5df3be
XGRJ78 <XGRJ78@XGRJ78-Z14G8>
2022-06-13 22:00:11 +0200

    void move

--- main.cpp
+++ main.cpp
@@ -1,73 +1,70 @@
-#include <string>
 #include <iostream>
-
-// osobne funkcje do poszczegolnych czynnosci (podnies, uzyj, idz(kierunek), ...)
-// HELP ZALEzNY OD OBECNEJ LOkalizacji (np. opis przedmiotow ktore sie tam znajduja)
-// warunek zwyciestwa uzaleznic od roznych rzeczy (meta, zebrane przedmioty itd)
-
-struct scene {
-	std::string description; 
-	std::string possible_directions[4];
-	// ... 
-};
-
+#include <cstring>
+#include <cctype>
+#include<conio.h>
+#include <string>
+char isValidDirect(std::string input);
+void goSomwhere(std::string direction);
+void move(int& current_scene_x, int& current_scene_y, char direction);
 struct player {
 	int current_scene_x;
 	int current_scene_y;
 	std::string name;
-	string name;
 };
-	
-
-scene scenes[2][4];
-
 int main() {
-
-	std::cout << "Czolem kluski z rosolem" << std::endl;
-	player player_1; // zmienna reprezentujaca gracza
-	player_1.current_scene_x = 0;
-	player_1.current_scene_y = 0;
-
-	int state = 0; // obecny stan (patrz: schemat stanów)
-
-	do
-	{
-		switch (state)
-		{
-		case 0: // stan SCENE
-
-			// wyswietl opis sceny (na podstawie wspolrzednych)
-			std::cout << scenes[player_1.current_scene_x][player_1.current_scene_y].description;
-			
-			// wystwietl dostepne opcje (na podstawie wspolrzednych)
-
-			
-			// zczytaj opcje wybrana przez uzytkownika (walidacja)
-			// users_choice = let_user_decide();
-
-			// zmien stan w zaleznosci od wybranej przez uzytkownika opcji
-			// state = users_choice;
-			break;
-
-		case 1: // np. stan NORTH
-
-			// zmniejsz wspolrzedna y o 1
-			//player.current_scene_y--;
-
-			// wroc do stanu SCENE
-			state = 0;
-			break;
-
-		case 2: // np. stan SOUTH
-
-			// zwieksz wspolrzedna y o 1
-
-			// wroc do stanu SCENE
-			break;
-
-		}
-
-	} while (false);
-
+	player player_1; // player_1 represents a player
+	player_1.current_scene_x = 1;
+	player_1.current_scene_y = 1;
+	std::string userInput;
+	std::cout << "Where you want to go (N)ORTH, (S)OUTH, (E)AST, (W)EST? " << std::endl;
+	std::cin >> userInput;
+	goSomwhere(userInput);
+	char direction = isValidDirect(userInput);
+	move(player_1.current_scene_x, player_1.current_scene_y, direction);
+	std::cout << player_1.current_scene_x << " " << player_1.current_scene_y;
+	getch();
 	return 0;
+}
+/*
+    check is input valid
+	//------- Possible move:
+	//	 North	(N)
+	//	 South	(S)
+	//	 East	(E)
+	//	 West	(W)
+
+ @return string with correct  direction or X when input is invalid
+*/
+char isValidDirect(std::string input) {
+	char output;
+	for (auto& element : input) { element = toupper(element); }
+	if (input == "N" || input == "NORTH") { output = 'N'; }
+	else if (input == "S" || input == "SOUTH") { output = 'S'; }
+	else if (input == "E" || input == "EAST") { output = 'E'; }
+	else if (input == "W" || input == "WEST") { output = 'W'; }
+	else { output = 'X'; }
+	return output;
+}
+// function works until user enters valid direction
+void goSomwhere(std::string direction) {
+	while (isValidDirect(direction) == 'X') {
+		std::cout << "You have chosen wrong direction. Try again" << std::endl;
+		std::cin >> direction;
+		isValidDirect(direction);
+	}
+	std::cout << "Good move." << std::endl;
+}
+void move(int& current_scene_x, int& current_scene_y, char direction) {
+	if (direction == 'N'/* && scene???.possible_directions.find("N")<4*/) {
+		current_scene_y--;
+	}
+	if (direction == 'S') {
+		current_scene_y++;
+	}
+	if (direction == 'E') {
+		current_scene_x++;
+	}
+	if (direction == 'W') {
+		current_scene_x--;
+	}
 }
