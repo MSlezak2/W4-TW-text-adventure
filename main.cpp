34860662acc096e7d2cf7d1424b980c6f596c8b5
salmaro <marcin.salamon@motorolasolutions.com>
2022-06-15 14:33:22 +0200

    add player_3

--- main.cpp
+++ main.cpp
@@ -47,6 +47,8 @@ void display_final_message();
 int main() {
 
 	player player_1; // zmienna reprezentujaca gracza
+	player player_3; 
+
 	player_1.current_scene_x = 0;
 	player_1.current_scene_y = 0;
 
